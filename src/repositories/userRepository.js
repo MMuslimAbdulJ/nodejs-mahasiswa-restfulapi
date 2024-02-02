7593b410b6a47d609cb6d08aa4e52c2d6bd02076
Muhammad Muslim Abdul Jabbaar <mmuslimabdulj@gmail.com>
2024-02-02 21:05:25 +0700

    remove redundant code from bug fixes

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,17 +1,12 @@
 const db = require("../database/db");
 
-function handleCallback(callback) {
-    return (error, results, fields) => {
+function save(user, callback) {
+    db.query("INSERT INTO users SET ?", user, (error, results, fields) => {
         if (error) {
             callback(error);
         }
-        console.log(results);
         return callback(null, results);
-    };
-}
-
-function save(user, callback) {
-    db.query("INSERT INTO users SET ?", user, handleCallback(callback));
+    });
 }
 
 function findOne(username, callback) {
